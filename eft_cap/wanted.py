cc7accd9cd32804756029cde3152648879e91ee2
Kirill Pinchuk <cybergrind@gmail.com>
2020-06-03 18:51:40 +0300

    loot: more wanted items

--- eft_cap/wanted.py
+++ eft_cap/wanted.py
@@ -2,16 +2,15 @@
 WANTED = {
     '590c621186f774138d11ea29',  # flashdrive
     '5c94bbff86f7747ee735c08f',  # lab key
-    '5af0548586f7743a532b7e99',  # ibuprofen
-    '5d1b371186f774253763a656',  # plastic fuel
+    # '5d1b371186f774253763a656',  # plastic fuel
     '5751a89d24597722aa0e8db0',  # golden star
-    '5755383e24597772cb798966',  # vaseline
+
 
     "5c1d0c5f86f7744bb2683cf0",  # "Lab. Blue keycard"
-    "5c1d0d6d86f7744bb2683e1f",  # "Lab. Yellow keycard. "
-    "5c1d0dc586f7744baf2e7b79",  # "Lab. Green keycard "
+    "5c1d0d6d86f7744bb2683e1f",  # "Lab. Green keycard. "
+    "5c1d0dc586f7744baf2e7b79",  # "Lab. Orange keycard "
     "5c1d0efb86f7744baf2e7b7b",  # "Lab. RED KEYCARD !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
-    "5c1d0f4986f7744bb01837fa",  # "Lab. Black keycard"
+    "5c1d0f4986f7744bb01837fa",  # "Lab. Yellow keycard"
     "5c1e2a1e86f77431ea0ea84c",  # "Lab. key. Manager office"
     "5c1e2d1f86f77431e9280bee",  # "Lab. key. Testing area (weap.)"
     "5c1e495a86f7743109743dfb",  # "Lab. Violet keycard"
@@ -20,9 +19,8 @@ WANTED = {
     "57347ca924597744596b4e71",  # "Graphics card"
     "5c12620d86f7743f8b198b72",  # "Tetriz"
     "5c0530ee86f774697952d952",  # "LEDX"
-    "5c110624d174af029e69734c",  # "T-7"
     "5c0126f40db834002a125382",  # "RR"
-    "59fb023c86f7746d0d4b423c",  # "WCase"
+
     "5d80cab086f77440535be201",  # "RB-MP22 key"
     "5d80cb5686f77440545d1286",  # "RB-PS81 key"
     "5d8e3ecc86f774414c78d05e",  # "RB-GN key"
@@ -32,12 +30,88 @@ WANTED = {
     "5a0ee34586f774023b6ee092",  # "West wing room 220 key"
     "5d80c6c586f77440351beef1",  # "RB-OB key"
 
-    "543be5e94bdc2df1348b4568",  # "Key"
+    "59e3639286f7741777737013",  # lion
+    "5b43575a86f77424f443fe62",  # fcond
+    "59e35cbb86f7741778269d83",  # hose
+    "5c052fb986f7746b2101e909",  # rfid rdr
+    "59e3606886f77417674759a5",  # nacl
+    "5af0534a86f7743b6f354284",  # med diagset
+    "5b4335ba86f7744d2837a264",  # med bloodset
+    "59e3658a86f7741776641ac4",  # cat
+    "5d1b313086f77425227d1678",  # ele relay
+    "5e54f6af86f7742199090bf3",  # lupo
+    "5d0379a886f77420407aa271",  # 30mm
+    "5c12688486f77426843c7d32",  # paracord
+    "5e2af29386f7746d4159f077",  # kektape
+    "5d1b327086f7742525194449",  # pressgauge
+    "5bc9b720d4351e450201234b",  # 1gphone
+    "5c052f6886f7746b1e3db148",  # wireless transm
+    "5d0375ff86f774186372f685",  # m.cable
+    "5d0376a486f7747d8050965c",  # electric militaryboard
+    "5d03784a86f774203e7e0c4d",  # gyrotachometer
+    "5d0378d486f77420421a5ff4",  # powerfilter
+    "5d0377ce86f774186372f689",  # thermalmodule
+    "5d03775b86f774203e7e0c4b",  # phaseantenna
+    "5d03794386f77420415576f5",  # military battery
+    "5e2aee0a86f774755a234b62",  # cyclon
+    "590a373286f774287540368b",  # flam. dry
+    "5d6fc78386f77449d825f9dc",  # green powder
+    "5bc9b9ecd4351e3bac122519",  # beardoil
+    "5d40419286f774318526545f",  # m.scissors
+    "5af04b6486f774195a3ebb49",  # pliers elite
+    "590de71386f774347051a052",  # teapot
+    "590de7e986f7741b096e5f32",  # "item_quest_barter_valuable_vase",
+    "5c1267ee86f77416ec610f72",  # "item_barter_valuable_prokill",
+    "5d235b4d86f7742e017bc88a",  # "item_barter_valuable_gp",
+    "5d235a5986f77443f6329bc6",  # "item_barter_valuable_skullring",
+    "5734758f24597738025ee253",  # "chain_gold",
+    "5bc9bc53d4351e00367fbcee",  # "item_barter_valuable_chicken",
+    "5bc9c377d4351e3bac12251b",  # "item_barter_valuable_kresalo",
+    "5e54f62086f774219b0f1937",  # "item_barter_valuable_raven",
+    "5bc9bdb8d4351e003562b8a1",  # "item_barter_valuable_elibadge",
+    "5aafbde786f774389d0cbc0f",  # "item_container_ammo",
+    "590c60fc86f77412b13fddcf",  # "item_info_docbag",
+    "59fb016586f7746d0d4b423a",  # "item_container_money",
+    "5d235bb686f77443f4331278",  # "item_container_lopouch",
+    "59fb023c86f7746d0d4b423c",  # "item_container_weaponcase",
+    "59f8a37386f7747af3328f06",  # "foregrip_all_fortis_shift",
+    "5c1bc5612e221602b5429350",  # "foregrip_all_zenit_rk_1",
+    "5a34fe59c4a282000b1521a2",  # "silencer_ar10_aac_762_sdn_6_762x51",
+    "59bffbb386f77435b379b9c2",  # "silencer_mount_silencerco_hybrid_46_multi",
+    "57ac965c24597706be5f975c",  # "scope_all_elcan_specter_dr_1-4",
+    "57aca93d2459771f2c7e26db",  # "scope_all_elcan_specter_dr_1-4_fde",
+    "544a3a774bdc2d3a388b4567",  # "scope_all_leupold_mark4_hamr",
+    "5d2dc3e548f035404a1a4798",  # "scope_all_monstrum_compact_prism_scope_2x32",
+    "5c0517910db83400232ffee5",  # "scope_all_valday_ps320",
+    "5b3b99475acfc432ff4dcbee",  # "scope_30mm_eotech_vudu_1_6x24",
+    "56ea70acd2720b844b8b4594",  # "scope_34mm_hensoldt_zf_4_16x56_ff",
+    "5dfe6104585a0c3e995c7b82",  # "scope_all_ncstar_advance_dual_optic_3_9x_42",
+    "5aa66be6e5b5b0214e506e97",  # "scope_34mm_nightforce_atacr_7_35x56",
+    "57235b6f24597759bf5a30f1",  # "nvg_pvs_14",
+    "5c066e3a0db834001b7353f0",  # "nvg_armasight_n-15",
+    "5d1b5e94d7ad1a2b865a96b0",  # "scope_all_flir_rs32_225_9x_35_60hz",
+    "5c0558060db834001b735271",  # "nvg_l3_gpnvg-18_anvis",
+    "5c110624d174af029e69734c",  # "thermal_spi_t7",
+    "5a1eaa87fcdbcb001865f75e",  # "scope_base_trijicon_reap-ir",
+    "55d482194bdc2d1d4e8b456b",  # "mag_ak74_izhmash_6L31_545x39_60",
+    "5c17664f2e2216398b5a7e3c",  # "handguard_ak_vltor_cmrd",
+    "59db3a1d86f77429e05b4e92",  # "pistolgrip_ar15_naroh_arms_gral_s",
+    "5d025cc1d7ad1a53845279ef",  # "pistolgrip_ar15_hk_ergo_psg1_style_grip",
+    "58d3db5386f77426186285a0",  # "weapon_grenade_m67",
+    "5c0e531286f7747fa54205c2",  # "SJ1_TGLabs",
+    "5c0e534186f7747fa1419867",  # "eTG_change",
+    "5d02778e86f774203e7dedbe",  # "core_medical_surgical_kit",
+    "5d02797c86f774203f38e30a",  # "survival_first_aid_rollup_kit",
+    "5755383e24597772cb798966",  # "vaseline",
+    "5e42c83786f7742a021fdf3c",  # "keycard_inter_store2",
+    "5c12613b86f7743bbe2c3f76",  # "item_info_intelligence",
+    "590c392f86f77444754deb29",  # "item_barter_electr_ssd",
+    "5c05300686f7746dce784e5d",  # "Item_barter_electr_flashstorage",
+
     "5448ba0b4bdc2d02308b456c",  # "Factory exit key"
     "5780cf7f2459777de4559322",  # "Marked key"
     "5780d0532459777a5108b9a2",  # "Customs office key"
 
-    # "590c62a386f77412b0130255",  # "Sliderkey Secure Flash drive"
     "5913915886f774123603c392",  # "Military base checkpoint key"
     "59148f8286f7741b951ea113",  # "Weapon safe key"
     "5a0448bc86f774736f14efa8",  # "The key to the closed premises of the sanatorium"
@@ -46,7 +120,6 @@ WANTED = {
     "5a0ea64786f7741707720468",  # "Office 107 East wing key"
     "5a0ea69f86f7741cd5406619",  # "Office 108 East wing key"
     "5a0ea79b86f7741d4a35298e",  # "Health resort utility room key"
-    # "5a0eb38b86f774153b320eb0",  # "SMW car key"
     "5a0eb6ac86f7743124037a28",  # "Cottage back entrance key"
     "5a0ec6d286f7742c0b518fb5",  # "West wing room 205 key"
     "5a0ec70e86f7742c0b518fba",  # "West wing room 207 key"
@@ -84,7 +157,6 @@ WANTED = {
     "5a145d4786f7744cbb6f4a12",  # "East wing room 306 key"
     "5a145d7b86f7744cbb6f4a13",  # "East wing room 308 key"
     "5a145ebb86f77458f1796f05",  # "East wing room 316 key"
-    # "5a29357286f77409c705e025",  # "Sliderkey Flash drive"
     "5ad5ccd186f774446d5706e9",  # "Key to OLI administrator office"
     "5ad5cfbd86f7742c825d6104",  # "Key to OLI logistics department office"
     "5ad5d20586f77449be26d877",  # "Key to utility room of OLI outlet"
